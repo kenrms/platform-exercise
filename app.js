9c741834496feaa1f7a1b0048440bf13ecd57a3a
Ken Ramos <vonnduven@gmail.com>
2020-01-29 19:56:12 -0800

    patch and delete implemented; minor cleanup

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const User = require('./models/userModel');
+const userRouter = require('./routes/userRouter');
+
 const app = express();
 const db = mongoose.connect('mongodb://localhost/userAPI', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if (!err) {
@@ -11,16 +14,13 @@ const db = mongoose.connect('mongodb://localhost/userAPI', { useNewUrlParser: tr
 });
 const port = process.env.PORT || 3000;
 
-const User = require('./models/userModel')
-const userRouter = require('./routes/userRouter')(User);
-
 mongoose.Promise = Promise;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/api', userRouter);
+app.use('/api', userRouter(User));
 
 app.listen(port, () => {
   console.log(`Running on port ${port}...`);
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,24 +2,61 @@ const express = require('express');
 const jwt = require('jwt-simple');
 const bcrypt = require('bcrypt-nodejs');
 
+const TOKEN_SECRET = '123'; // TODO this should come from env or some config
+
 function routes(User) {
+  function checkAuth(req, res, next) {
+    if (!req.header('authorization')) {
+      return res.status(401).json({ message: 'Authorization header missing' });
+    }
+
+    try {
+      var token = req.header('authorization').split(' ')[1];
+      var payload = jwt.decode(token, TOKEN_SECRET);
+
+      if (!payload) {
+        return res.status(401).json({ message: 'Invalid Authorization header' });
+      }
+
+      req.userId = payload.sub;
+
+      return next();
+    }
+    catch (ex) {
+      console.error(ex);
+      return res.status(500).json({ message: ex.message });
+    }
+  }
+
+  function generateToken(user) {
+    var payload = { sub: user._id };
+    return jwt.encode(payload, TOKEN_SECRET);
+  }
+
+  function validateNewUser(userData) {
+    // TODO validation
+    // name - not empty, minimum length
+    // email - length and regex to validate email format, ensure email doesn't already exist
+    // password - business rules and restrictions (length, character whitelist, minimum strength)
+    return true;
+  }
+
   const router = express.Router();
 
   router.route('/register')
     .post((req, res) => {
       const user = new User(req.body);
 
-      // TODO validation and hashing password
-      // name - not empty, minimum length
-      // email - regex to validate email format
-      // password - business rules and restrictions (length, character whitelist, minimum strength)
+      if (!validateNewUser(user)) {
+        res.status(400).send({ message: "Invalid registration data" });
+      }
 
-      user.save((err, result) => {
+      user.save((err, newUser) => {
         if (err) { return res.send(err); }
 
-        return res.status(201).json(user);
+        return res.status(201).json({ token: generateToken(newUser) });
       });
-    })
+    });
 
   router.route('/login')
     .post(async (req, res) => {
@@ -28,90 +65,70 @@ function routes(User) {
       var user = await User.findOne({ email: loginData.email });
 
       if (!user) {
-        return res.sendStatus(401).send({ message: 'Email invalid' });
+        return res.status(401).send({ message: 'Email invalid' });
       }
 
       bcrypt.compare(loginData.password, user.password, (err, isMatch) => {
         if (err) {
-          return res.sendStatus(500);
+          return res.status(500).json({ message: 'Error comparing passwords' });
         }
 
         if (!isMatch) {
-          return res.sendStatus(401);
+          return res.status(401).json({ message: 'Password invalid' });
         }
 
-        // generate token
-        var payload = {};
-        var token = jwt.encode(payload, 'mysecret');  // secret should come from config or something (env?)
+        return res.json({ token: generateToken(user) });
+      });
+    });
 
-        return res.json({ token });
+  router.route('/users')
+    .patch(checkAuth, async (req, res) => {
+      await User.findById(req.userId, (findError, user) => {
+        if (findError) {
+          res.send(400).json({ message: 'User not found' })
+        }
+
+        if (req.body._id) {
+          delete req.body._id;
+        }
+
+        // Actually update model
+        Object.entries(req.body).forEach((item) => {
+          const key = item[0];
+          const value = item[1];
+
+          user[key] = value;
+        });
+
+        user.save((saveError) => {
+          if (saveError) {
+            return res.send(saveError);
+          }
+          return res.sendStatus(204);
+        });
       });
     })
+    .delete(checkAuth, async (req, res) => {
 
-  // debug -- check list of users
-  router.route('/users')
+      await User.findById(req.userId, (findError, user) => {
+        if (findError || !user) {
+          return res.status(400).json({ message: 'User not found' })
+        }
+
+        user.remove((removeError) => {
+          if (removeError) {
+            return res.send(removeError);
+          }
+
+          return res.status(204).json({ message: 'User deleted successfully' });
+        });
+      });
+    })
+    // TODO debug -- check list of users
     .get(async (req, res) => {
       await User.find({}, '-password -__v', (err, user) => {
         if (err) { return res.send(err); }
         return res.json(user);
-      });
-    });
-
-  // Middleware to find a user by id
-  router.use('/users/:userId', (req, res, next) => {
-    User.findById(req.params.userId, (err, user) => {
-      if (err) { return res.send(err); }
-      if (user) {
-        req.user = user;
-        return next();
-      }
-      return res.sendStatus(404);
-    });
-  });
-
-  router.route('/users/:userId')
-    .get((req, res) => res.json(req.user))
-    .put((req, res) => {
-      const { user } = req;
-
-      user.name = req.body.name;
-      user.email = req.body.email;
-      user.password = req.body.password;
-      // TODO other fields and validation for updating
-
-      req.user.save((err) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(user);
-      });
-    })
-    .patch((req, res) => {
-      const { user } = req;
-
-      if (req.body._id) {
-        delete req.body._id;
-      }
-
-      Object.entries(req.body).forEach((item) => {
-        const key = item[0];
-        const value = item[1];
-        user[key] = value;
-      });
-
-      req.user.save((err) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(user);
-      });
-    })
-    .delete((req, res) => {
-      req.user.remove((err) => {
-        if (err) {
-          return res.send(err);
-        }
-        res.sendStatus(204);
       });
     });
 
